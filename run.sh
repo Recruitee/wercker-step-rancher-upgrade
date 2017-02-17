acdb8419a2ac4aff91a82de86a28e1dbd98bd30d
Tymon Tobolski <i@teamon.eu>
2017-02-17 10:34:30 +0100

    Use functions

--- run.sh
+++ run.sh
@@ -9,73 +9,81 @@ SERVICE_ID=$WERCKER_RANCHER_UPGRADE_SERVICE_ID
 IMAGE=$WERCKER_RANCHER_UPGRADE_IMAGE
 TIMEOUT=${WERCKER_RANCHER_UPGRADE_TIMEOUT:-"60"}
 
+
 get(){
   curl -s --user "$ACCESS_KEY:$SECRET" "$URL/v1/services/$SERVICE_ID"
 }
 
+upgrade(){
+  info "Upgrading"
 
-service=$(get)
-state=$(echo $service | jq -r '.state')
+  up_url=$(echo $service | jq -r '.actions.upgrade')
+  up_data=$(echo $service | jq -r --arg img "docker:$IMAGE" '
+      .upgrade.inServiceStrategy
+    | .launchConfig.imageUuid = $img
+    | .startFirst = true
+    | del(.previousSecondaryConfigs)
+    | del(.previousSecondaryLaunchConfigs)
+    | {inServiceStrategy: .}
+  ')
 
-case $state in
-  active)
-    info "---> Upgrading"
+  curl -i -s --user "$ACCESS_KEY:$SECRET" -X POST "$up_url" -H 'Content-Type: application/json' -d "$up_data"
 
-    up_url=$(echo $service | jq -r '.actions.upgrade')
-    up_data=$(echo $service | jq -r --arg img "docker:$IMAGE" '
-        .upgrade.inServiceStrategy
-      | .launchConfig.imageUuid = $img
-      | .startFirst = true
-      | del(.previousSecondaryConfigs)
-      | del(.previousSecondaryLaunchConfigs)
-      | {inServiceStrategy: .}
-    ')
+  while [ "$TIMEOUT" -gt "0" ]; do
+    service=$(get)
+    state=$(echo $service | jq -r '.state')
 
-    curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$up_url" -H 'Content-Type: application/json' -d "$up_data" > /dev/null
+    case $state in
+      upgraded)
+        info "Upgraded"
+        finish_url=$(echo $service | jq -r '.actions.finishupgrade')
+        curl -i -s --user "$ACCESS_KEY:$SECRET" -X POST "$finish_url"
+        success "Finished"
+        return
+        ;;
 
-    while [ "$TIMEOUT" -gt "0" ]; do
-      service=$(get)
-      state=$(echo $service | jq -r '.state')
+      *)
+        info "---> Waiting ($TIMEOUT) - $state"
+        TIMEOUT=$((TIMEOUT - 1))
+        sleep 1
+        ;;
+    esac
+  done
 
-      case $state in
-        upgraded)
-          info "Upgraded"
-          finish_url=$(echo $service | jq -r '.actions.finishupgrade')
-          curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$finish_url" > /dev/null
-          success "Finished"
-          exit
-          ;;
+  info "Upgrade timeout. Cancel upgrade"
+  cancel_url=$(echo $service | jq -r '.actions.cancelupgrade')
+  curl -i -s --user "$ACCESS_KEY:$SECRET" -X POST "$cancel_url"
 
-        *)
-          info "---> Waiting ($TIMEOUT) - $state"
-          TIMEOUT=$((TIMEOUT - 1))
-          sleep 1
-          ;;
-      esac
-    done
+  while true; do
+    service=$(get)
+    state=$(echo $service | jq -r '.state')
 
-    info "Upgrade timeout. Cancel upgrade"
-    cancel_url=$(echo $service | jq -r '.actions.cancelupgrade')
-    curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$cancel_url" > /dev/null
+    case $state in
+      canceling-upgrade)
+        info "Canceling upgrade"
+        sleep 1
+        ;;
 
-    while true; do
-      service=$(get)
-      state=$(echo $service | jq -r '.state')
+      *)
+        fail "Upgrade cancelled"
+        ;;
+    esac
+  done
+}
 
-      case $state in
-        canceling-upgrade)
-          info "Canceling upgrade"
-          sleep 1
-          ;;
+main(){
+  service=$(get)
+  state=$(echo $service | jq -r '.state')
 
-        *)
-          fail "Upgrade cancelled"
-          ;;
-      esac
-    done
-    ;;
+  case $state in
+    active)
+      upgrade
+      ;;
 
-  *)
-    fail "Can't upgrade, service $SERVICE_ID is $state"
-    ;;
-esac
+    *)
+      fail "Can't upgrade, service $SERVICE_ID is $state"
+      ;;
+  esac
+}
+
+main
