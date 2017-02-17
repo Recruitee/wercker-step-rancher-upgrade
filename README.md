dd36d813710c6af38f5b1fb303695574f175b444
Tymon Tobolski <i@teamon.eu>
2017-02-17 10:15:04 +0100

    Use wercker shell api

---
+++ README.md
@@ -0,0 +1,5 @@
+## Testing
+
+```
+docker run --rm -it -v $(pwd):/app alpine:3.4 /app/run.sh
+```
--- run.sh
+++ run.sh
@@ -9,16 +9,17 @@ SERVICE_ID=$WERCKER_RANCHER_UPGRADE_SERVICE_ID
 IMAGE=$WERCKER_RANCHER_UPGRADE_IMAGE
 TIMEOUT=${WERCKER_RANCHER_UPGRADE_TIMEOUT:-"60"}
 
-_get (){
+get(){
   curl -s --user "$ACCESS_KEY:$SECRET" "$URL/v1/services/$SERVICE_ID"
 }
 
-service=$(_get)
+
+service=$(get)
 state=$(echo $service | jq -r '.state')
 
 case $state in
   active)
-    echo "---> Upgrading" >&2
+    info "---> Upgrading"
 
     up_url=$(echo $service | jq -r '.actions.upgrade')
     up_data=$(echo $service | jq -r --arg img "docker:$IMAGE" '
@@ -33,49 +34,48 @@ case $state in
     curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$up_url" -H 'Content-Type: application/json' -d "$up_data" > /dev/null
 
     while [ "$TIMEOUT" -gt "0" ]; do
-      service=$(_get)
+      service=$(get)
       state=$(echo $service | jq -r '.state')
 
       case $state in
         upgraded)
-          echo "---> Upgraded"
+          info "Upgraded"
           finish_url=$(echo $service | jq -r '.actions.finishupgrade')
           curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$finish_url" > /dev/null
-          echo "---> Finished"
-          exit 0
+          success "Finished"
+          exit
           ;;
 
         *)
-          echo "---> Waiting ($TIMEOUT) - $state" >&2
+          info "---> Waiting ($TIMEOUT) - $state"
           TIMEOUT=$((TIMEOUT - 1))
           sleep 1
           ;;
       esac
     done
 
-    echo "---> Upgrade timeout. Cancel upgrade" >&2
-    echo $service
+    info "Upgrade timeout. Cancel upgrade"
     cancel_url=$(echo $service | jq -r '.actions.cancelupgrade')
     curl -s --user "$ACCESS_KEY:$SECRET" -X POST "$cancel_url" > /dev/null
 
     while true; do
-      service=$(_get)
+      service=$(get)
       state=$(echo $service | jq -r '.state')
 
       case $state in
         canceling-upgrade)
-          echo "---> Canceling upgrade"
+          info "Canceling upgrade"
           sleep 1
           ;;
 
         *)
-          exit 2
+          fail "Upgrade cancelled"
           ;;
       esac
     done
     ;;
 
   *)
-    echo "---> ERROR: Can't upgrade, service $SERVICE_ID is $state"
+    fail "Can't upgrade, service $SERVICE_ID is $state"
     ;;
 esac
